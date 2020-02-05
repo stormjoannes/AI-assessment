8269e99e3bbdb243cdb25f1a279fb8c2ce903ce7
obayy <obay.elkoukouchi@student.hu.nl>
2020-02-05 22:27:37 +0100

    pyramide

--- algoritmicprog.py
+++ algoritmicprog.py
@@ -1,1 +1,39 @@
-print("Het is gelukt!!!!!!!")
+
+def naam():
+    n = int(input("hoe groot?"))
+    t = 0
+
+    for i in range((n * 2) - 1):
+        if i < n:
+            t += 1
+        else:
+            t -= 1
+
+        for j in range(t):
+            print("*", end="")
+        print("")
+
+
+
+naam()
+
+def andereNaam():
+    n = int(input("hoe groot? "))
+    t = 0
+    i = 0
+    j = 0
+
+    while i < (n * 2) - 1:
+        if i < n:
+            t += 1
+        else:
+            t -= 1
+
+        while j < t:
+            print("*", end="")
+            j += 1
+        print("")
+        i += 1
+        j = 0
+
+andereNaam()
